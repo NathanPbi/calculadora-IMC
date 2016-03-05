0aa8661c6104d0969b2735c97201a143041037ee
jonatas luis <jonatasluis.unipe@gmail.com>
2016-03-05 13:04:18 -0300

    new commit

--- app/src/main/java/controller/Calc_Imc_Controller.java
+++ app/src/main/java/controller/Calc_Imc_Controller.java
@@ -9,24 +9,14 @@ import model.Calc_Imc_Model;
  */
 public class Calc_Imc_Controller {
 
-    //variavel model
     Calc_Imc_Model calc_model = new Calc_Imc_Model();;
 
-
-
     public double calcularIMC(double peso, double altura){
-
         double imc = this.calc_model.calcularIMC(peso, altura);
-
         return imc;
-
     }
 
     public String mensagemAoUsuario(){
         return this.calc_model.mensagemAoUsuario();
     }
-
-
-
-
 }
